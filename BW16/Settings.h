bdb91674f723c5b41f57599ae3ae4fb9dfc4b248
warwick320 <sihu687@gmail.com>
2025-06-07 21:56:16 +0900

    Change logo animation

--- BW16/Settings.h
+++ BW16/Settings.h
@@ -2,11 +2,15 @@
 #undef min
 #include "Display.h"
 //#include ""
-
+void logo_animate(){
+  for(int i=0; i< Logo_gif_allArray_LEN; i++){
+    display.drawBitmap(0,0,Logo_gif_allArray[i],128,64,SSD1306_WHITE);
+    display.display();
+    delay(45); // 장수 마다 다름 장수 나누기 60 나온 값에 10곱
+  }
+}
 void bw_16_set_up(){
   Serial.begin(115200);
-  setupButtons();
-  //Wire.begin();
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 F"));
     while (true)
@@ -15,11 +19,14 @@ void bw_16_set_up(){
 
   display.clearDisplay();
   display.setTextSize(1);
-  display.drawBitmap(0,0,LOGO,128,64,SSD1306_WHITE);
-  display.display();
-  delay(500);
+  logo_animate();
+  // display.drawBitmap(0,0,LOGO,128,64,SSD1306_WHITE);
+  // display.display();
+  delay(1500);
+  setupButtons();
+  //Wire.begin(); 버그 있음 3.1.8버전
   main_names = {"Attack", "Scan", "Select","Settings"};
-  at_names = {"Deauth", "All Deauth","Beacon","Authentication","Association","Evil Twin","Sour Apple"};
+  at_names = {"Deauth", "All Deauth","Becaon","Authentication","Association"};
   Serial.println("done");
 
   // if (scanNetworks("Booting") != 0) {
